06478e18d0ad856288928a82c0706bca99f49bef
Juan Pablo <juan.tentiomaru9309@mail.com>
2021-03-04 18:55:46 -0300

    Parte 1 corrigida

--- exercises/exercises-bloco5/javascript-dom-eventos-e-web-storage_2/.history/script_20210304185040.js
+++ exercises/exercises-bloco5/javascript-dom-eventos-e-web-storage_2/.history/script_20210304185040.js
@@ -1,8 +1,8 @@
 //parte1
 let classH1 = document.querySelector('.h1Classe') 
 let h1Filho = document.createElement('h1')
-h1Filho.innerText = 'Exercício 5.2 - JavaScript DOM'
-document.body.appendChild(h1Filho)
+h1Filho.innerText'Exercício 5.2 - JavaScript DOM')
+classH1.appendChild(h1Filho)
 
 //parte2
 
--- exercises/exercises-bloco5/javascript-dom-eventos-e-web-storage_2/.history/script_20210304185226.js
+++ exercises/exercises-bloco5/javascript-dom-eventos-e-web-storage_2/.history/script_20210304185226.js
@@ -2,7 +2,7 @@
 let classH1 = document.querySelector('.h1Classe') 
 let h1Filho = document.createElement('h1')
 h1Filho.innerText = 'Exercício 5.2 - JavaScript DOM'
-document.body.appendChild(h1Filho)
+document.bodyappendChild(h1Filho)
 
 //parte2
 
